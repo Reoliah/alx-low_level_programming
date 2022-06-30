5856f5b525c807065edec36aa67dc1971e0ee142
Isaiah Omoboriowo <tenidaisy@gmail.com>
2022-06-30 09:19:48 -0700

    using puts() in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,3 @@
 # include <stdio.h>
-int main ( ) {puts("Programming is like building a multilingual puzzle") return 0}
+int main ( ) {puts("Programming is like building a multilingual puzzle";
+) return 0;}
