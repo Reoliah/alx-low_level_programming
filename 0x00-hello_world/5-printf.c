eb2a1d2d486b5ee582723b0e2719a21e0f8134d1
Isaiah Omoboriowo <tenidaisy@gmail.com>
2022-06-30 10:44:45 -0700

    printf in C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,9 @@
 /**
  *  * main - to be called
  *   * return:0 for the int
- *    */
-
+ */
 int main(void)
 {
-	        printf("with proper grammar, but the outcome is a piece of art,\n");
-		        return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
