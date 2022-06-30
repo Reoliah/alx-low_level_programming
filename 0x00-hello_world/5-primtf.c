3cc86d03583a866373b4aceb81928bfd4b8a6861
Isaiah Omoboriowo <tenidaisy@gmail.com>
2022-06-30 09:53:56 -0700

    printf in C

---
+++ 0x00-hello_world/5-primtf.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ *  * main - to be called
+ *   * return:0 for the int
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
