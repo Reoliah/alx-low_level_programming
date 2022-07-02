47b032daeb99044e5ee10d1ddc8811f5073b4168
Isaiah Omoboriowo <tenidaisy@gmail.com>
2022-07-01 21:08:18 -0700

    printing combinations of digits in C

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - combination of three digits
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b, c;
+	for (a = 48; a <= 51; a++)
+	{for (b = 48; b <= 51; b++)
+	{for (c = 48; c <= 51; c++)
+		{putchar(a);
+		putchar(b);
+		putchar(c);
+		if (a != 9)
+		{putchar(44);
+		putchar(32);
+		}
+		}
+	}
+	}
+putchar(10)
+	return (0);
+}
