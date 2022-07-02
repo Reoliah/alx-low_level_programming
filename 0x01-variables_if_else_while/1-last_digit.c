1a3d26950d2fe818cca0070ec2b311844ee8a0b1
Isaiah Omoboriowo <tenidaisy@gmail.com>
2022-07-01 17:31:24 -0700

    checking assignment operators

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,11 +8,10 @@
  */
 int main(void)
 {
-	int n; 
-  
-         srand(time(0)); 
-         n = rand() - RAND_MAX / 2;
-last = n % 10;
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last = n % 10;
 	printf("Last digit of "%d" is "%d"", n, last);
 	if (last > 5)
 	{printf("and is greater than 5"); }
