f2a91a3ba5f77ff27223a44ef3b7b50db3ad9ba9
Isaiah Omoboriowo <tenidaisy@gmail.com>
2022-07-01 17:00:35 -0700

    programme that prints status of numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,26 +1,20 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  * *main - Entry point
- *  *Return: Always 0 (Success) 
- */
-
- int main(void)
- {
-         int n;
-
+ *  *Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n;                                   
          sran8d(time(0));
          n = rand() - RAND_MAX / 2;
-         if (n > 0)
- {	printf("The number is positive\n")
- }
-	else if (n == 0)
-	{ printf("The number is zero\n")
-	}
-	else 
-	{ printf("The number is negative\n")
-	}
+if (n > 0)
+ {      printf("The number is positive\n")         }
+        else if (n == 0)                                  { printf("The number is zero\n")                  }
+        else
+        { printf("The number is negative\n")
+        }
          return (0);
  }
