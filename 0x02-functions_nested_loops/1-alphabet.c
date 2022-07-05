d1a67f7be08898569ed78d81c6ec9097f5ba0363
Isaiah Omoboriowo <tenidaisy@gmail.com>
2022-07-05 13:04:11 -0700

    printing lower case alphabets with _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - print all alphabets in lower case followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	int y;
+
+	for (y = a; y <= z; a++)
+	{_putchar(y);
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/2-alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - print all alphabets in lower case, ten times, followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 'a'; y <= 'z'; y++)
+		{
+			_putchar(y);
+		}
+		_putchar('\n');
+	}
+}
