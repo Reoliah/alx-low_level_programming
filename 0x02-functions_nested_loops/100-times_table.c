059a335db99f32c4593566fd6f9dd8c41c3118fe
Isaiah Omoboriowo <tenidaisy@gmail.com>
2022-07-05 19:14:11 -0700

    advanced usage of functions, loops and nested values

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,45 +6,42 @@
  */
 void print_times_table(int n)
 {
-	int x, y, product;
+	int i, j, k;
 
 	if (n >= 0 && n <= 15)
 	{
-		for (x = 0; x <= n; x++)
+		for (i = 0; i <= n; i++)
 		{
-			for (y = 0; y <= n; y++)
+			for (j = 0; j <= n; j++)
 			{
-				product = x * y;
-				if (y == 0)
+				k = j * i;
+				if (j == 0)
 				{
-					_putchar(product + '0');
-				}
-				else if (product < 10 && y != 0)
-				{
-					_putchar('.');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(product + '0');
-				}
-				else if (product >= 10 && product < 100)
+					_putchar(k + '0');
+				} else if (k < 10 && j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar((product / 10) + '0');
-					_putchar((product % 10) + '0');
-				}
-				else if (product >= 100)
+					_putchar(' ');
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
 				{
 					_putchar(',');
-				_putchar(' ');
-				_putchar((product / 100) + '0');
-				_putchar(((product/10) % 10) + '0');
-				_putchar((product % 10) + '0');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
 			}
-			
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
