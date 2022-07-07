ac522ffe60fc6d37abe13b31a52042da3813a35b
Isaiah Omoboriowo <tenidaisy@gmail.com>
2022-07-07 07:46:53 -0700

    more functions and nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - check if c  is upper case or lower
+ * @c: number being tested
+ * Return: Always 0
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
