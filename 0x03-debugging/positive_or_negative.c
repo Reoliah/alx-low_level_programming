5c1c6adffdd6cb0a5a1b294114531cf2e32dd7de
Isaiah Omoboriowo <tenidaisy@gmail.com>
2022-07-06 19:02:22 -0700

    act of debugging

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+void positive_or_negative(int i)
+{
+	if (i == 0)
+	{
+		printf("%d is zero\n", i)
+	}
+	else if (i > 0)
+	{
+		print("%d is positive\n", i)
+	}
+	else
+	{
+		print("%d is negative\n", i)
+	}
+}
