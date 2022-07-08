e98d58c68cff3f14997df39bb365e9418f385036
Isaiah Omoboriowo <tenidaisy@gmail.com>
2022-07-08 08:15:15 -0700

    loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,14 +5,14 @@
  */
 void print_most_numbers(void)
 {
-	char = 0;
 	int = i;
+	i = 0;
 
-	while (i < '9')
+	while (i < 10)
 	{
-		if ((i != '2') && (i != '4'))
+		if ((i != 2) && (i != 4))
 		{
-			_putchar(i);
+			_putchar(i + '0');
 		}
 		i++;
 	}
