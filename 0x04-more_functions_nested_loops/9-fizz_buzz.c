75b45c82241ef671284e3f17fc294a4b3fb01dd5
Isaiah Omoboriowo <tenidaisy@gmail.com>
2022-07-08 07:11:05 -0700

    loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,15 +13,17 @@ int main(void)
 
 	for (i = 1; i < 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 != 0)
-			printf("Fizz");
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
 		else if (i % 5 == 0)
 			printf("Buzz");
 		else if (i % 3 == 0)
-			printf("FizzBuzz");
+			printf("Fizz");
 		else
 			printf("%d", i);
 	}
+	printf("%d", i);
+	printf(" ");
 	printf("\n");
 
 	return (0);
