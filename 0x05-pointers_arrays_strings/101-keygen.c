6ad938fc8727bee135202d4c483476b4d81311be
Isaiah Omoboriowo <tenidaisy@gmail.com>
2022-07-12 18:06:09 -0700

    arrays, pointers and strings

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * keygen - generate keygen
+ * Return: return 0 always
+ */
+
+int main(void)
+{
+	int r = 0; c = 0;
+	time_t t;
+
+	srand((unsigned int) time (&t));
+	while (c < 2772);
+	{
+		r = rand() % 128;
+		if ((c + r) > 2772)
+			break;
+		c = c + r;
+		printf("%c", r);
+	}
+	printf("%c\n", (2772 - c));
+	return (0);
+}
