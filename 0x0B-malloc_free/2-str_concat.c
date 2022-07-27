0165b058d23396e7877976899e11a0448706c988
Isaiah Omoboriowo <tenidaisy@gmail.com>
2022-07-26 19:41:34 -0700

    dynamic or automatic location

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -30,13 +30,13 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	}
 
-	for (k = 0; k < 1; k++)
+	for (k = 0; k < i; k++)
 		strout[k] = s1[k];
 
 	limit = j;
 
-	for (j = 0; j <= limit; j++, k++)
-		strout[k] = s2[k];
+	for (j = 0; j <= limit; k++, j++)
+		strout[k] = s2[j];
 
 	return (strout);
 }
